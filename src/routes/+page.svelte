fb829f1331cd4c6acfdcf234f2c0f209281a1776
Jacob Jordan <31334963+Naturealizor@users.noreply.github.com>
2022-12-10 15:01:47 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<!-- create a template for a price comparison website -->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
